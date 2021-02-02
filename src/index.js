4285dd3027fc04bb86e4a0127cbc563831c25547
Alejandro Monasterios <alemonas@gmail.com>
2021-02-02 07:29:35 -0400

    add comments for module content on the index file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import nav from './nav';
 import { top, bottom } from './footer';
 import makeButton from './button';
+import { makeColorStyle } from './button-style';
 
-console.log(nav(), top, bottom, makeButton('create'));
+console.log(nav(), top, bottom, makeButton('create'), makeColorStyle('cyan'));
+
+/**
+ * First Module: Webpack from Scratch
+ *
+ * using webpack for first time
+ * Adding npm scripts for environment builds
+ * setting up debugging
+ * Coding your first Module
+ * Adding Watch mode
+ * ES Module Syntax
+ * CommonJS Export
+ * CommonJS Named Exports
+ * Tree Shaking (dead code elimination)
+ * Webpack Bunlde Walktrough
+ * Add webpack configuration example
+ */
