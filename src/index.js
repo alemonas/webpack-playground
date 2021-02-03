e34570b7836e801f3e71abc858d0db195c72f216
Alejandro Monasterios <alemonas@gmail.com>
2021-02-03 14:35:12 -0400

    update part 1 content

--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ import { makeColorStyle } from './button-style';
 console.log(nav(), top, bottom, makeButton('create'), makeColorStyle('cyan'));
 
 /**
- * First Module: Webpack from Scratch
+ * FIRST PART: Webpack from Scratch
  *
- * using webpack for first time
- * Adding npm scripts for environment builds
- * setting up debugging
- * Coding your first Module
- * Adding Watch mode
- * ES Module Syntax
- * CommonJS Export
- * CommonJS Named Exports
- * Tree Shaking (dead code elimination)
- * Webpack Bunlde Walktrough
- * Add webpack configuration example
+ * 01. using webpack for first time
+ * 02. Adding npm scripts for environment builds
+ * 03. setting up debugging
+ * 04. Coding your first Module
+ * 05. Adding Watch mode
+ * 06. ES Module Syntax
+ * 07. CommonJS Export
+ * 08. CommonJS Named Exports
+ * 09. Add Prettier (format code)
+ * 10. Tree Shaking (dead code elimination)
+ * 11. Webpack Bunlde Walktrough
+ * 12. Add webpack configuration example
+ * 13. Explain webpack main.js generated file on development
  */
