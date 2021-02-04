b55e6051655f15d6bd9a61cb4250e81b4548c2f0
Alejandro Monasterios <alemonas@gmail.com>
2021-02-04 18:56:43 -0400

    add style color to button

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import makeButton from './button';
 import { makeColorStyle } from './button-style';
 
 const button = makeButton('submit');
+button.style = makeColorStyle('blue');
 document.body.appendChild(button);
 
 console.log(nav(), top, bottom, makeColorStyle('cyan'));
