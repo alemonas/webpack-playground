0d7af27232281bb0d5d93a324d935630f34ea2dd
Alejandro Monasterios <alemonas@gmail.com>
2021-02-05 07:31:08 -0400

    clean and fix webpack main config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env);
+const { merge: webpackMerge } = require('webpack-merge');
 
-module.exports = (env) => {
-  console.log(env);
-  return {
-    mode: env.mode,
-    output: {
-      filename: 'bundle.js',
+// module.exports = (env) => {
+module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
+  console.log(mode);
+  return webpackMerge(
+    {
+      mode,
+      output: {
+        filename: 'bundle.js',
+      },
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
     },
-    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
-  };
+    modeConfig(mode)
+  );
 };
